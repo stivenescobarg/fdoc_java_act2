df59d23210332db0ec006be53ac0952949346c77
stivenescobarg <stivenescobar240208@gmail.com>
2025-10-30 15:19:31 -0500

    "WHILE"

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,41 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("==== EJERCICIOS CON WHILE ====");
+
+        System.out.println("== Números del 1 al 10 ==");
+        int contador = 1;
+        while (contador<=10) {
+            System.out.println(contador);
+            contador++;
+        }
+
+        System.out.println("== Suma Acumulatíva ==");
+        int n = 5; 
+        int suma = 0;
+        int i = 1;
+        while (i <= n) {
+            suma += i;
+            i++;
+        }
+        System.out.println("La suma de 1 a " + n + " es: " + suma);
+
+        System.out.println("== Buscar Número de Arreglo ==");
+        int[] numeros = {2, 5, 8, 10, 15, 20};
+        int numeroBuscado = 10;
+        boolean encontrado = false;
+        int indice = 1;
+        while (indice < numeros.length && !encontrado) {
+            if (numeros[indice] == numeroBuscado) {
+                encontrado = true;
+                System.out.println("Número " + numeroBuscado + " encontrado en posición " + indice);
+            }
+            indice++;
+        }
+        if (!encontrado) {
+            System.out.println("Número " + numeroBuscado + " no encontrado");
+        }
+
     }
 }
